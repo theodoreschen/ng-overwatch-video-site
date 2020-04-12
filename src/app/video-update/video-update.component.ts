62187e8fcdcbd977c84597807f78baf068b8fa81
tc <theairportexplorer@gmail.com>
2020-04-12 08:19:08 -0400

    beginning work on VideoUpdateComponent

--- src/app/video-update/video-update.component.ts
+++ src/app/video-update/video-update.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { LoggerService } from '../logger.service';
+import { VideoMetadata } from '../video-metadata';
+import { VideoSearchCacheService } from '../video-search-cache.service';
 
 @Component({
   selector: 'app-video-update',
@@ -6,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./video-update.component.css']
 })
 export class VideoUpdateComponent implements OnInit {
+  selectedVideo: VideoMetadata;
+  videoSearchResults: VideoMetadata[];
 
-  constructor() { }
+  constructor(
+    private log: LoggerService,
+    private videoSearchCache: VideoSearchCacheService,
+  ) { }
 
   ngOnInit(): void {
+    this.videoSearchResults = this.videoSearchCache.cachedResults;
   }
 
+  videoSelectHandler (video: VideoMetadata) {
+    this.log.DEBUG("VideoUpdateComponent.videoSelectHandler", JSON.stringify(video));
+    this.selectedVideo = video;
+  }
 }
