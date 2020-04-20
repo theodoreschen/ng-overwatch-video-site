32a1f37e4ee17168068d5fc36c306d62dfff39f1
tc <theairportexplorer@gmail.com>
2020-04-20 14:32:48 -0400

    completed logger-widget unit tests

--- src/app/logger-widget/logger-widget.component.spec.ts
+++ src/app/logger-widget/logger-widget.component.spec.ts
@@ -1,16 +1,24 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoggerWidgetComponent } from './logger-widget.component';
+import { By } from '@angular/platform-browser';
+import { LoggerService } from '../logger.service';
 
 describe('LoggerWidgetComponent', () => {
+  let mockLogService: any;
   let component: LoggerWidgetComponent;
   let fixture: ComponentFixture<LoggerWidgetComponent>;
 
   beforeEach(async(() => {
+    mockLogService = jasmine.createSpyObj(["setLevel"]);
+
     TestBed.configureTestingModule({
-      declarations: [ LoggerWidgetComponent ]
+      declarations: [LoggerWidgetComponent],
+      providers: [
+        { provide: LoggerService, useValue: mockLogService }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -21,5 +29,15 @@ describe('LoggerWidgetComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('setGlobalLogLevel', () => {
+    it('should set global log level to correct level', () => {
+      let loggerWidget = fixture.debugElement.query(By.css("#log-level-setter"));
+      loggerWidget.triggerEventHandler("ngModelChange", "3")
+      fixture.detectChanges();
+
+      expect(mockLogService.setLevel).toHaveBeenCalledWith(3);
+    });
   });
 });
