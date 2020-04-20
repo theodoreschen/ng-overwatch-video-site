de3be313da62c658671616578984e1bf305fbd00
tc <theairportexplorer@gmail.com>
2020-04-20 14:33:24 -0400

    finished video add component unit tests

--- src/app/video-add/video-add.component.spec.ts
+++ src/app/video-add/video-add.component.spec.ts
@@ -5,6 +5,7 @@ import { VideoService } from '../video.service';
 import { LoggerService } from '../logger.service';
 import { FormsModule } from '@angular/forms';
 import { By } from '@angular/platform-browser';
+import { of } from 'rxjs';
 
 describe('VideoAddComponent', () => {
   let mockLogService, mockVideoService;
@@ -13,17 +14,17 @@ describe('VideoAddComponent', () => {
 
   beforeEach(async(() => {
     mockLogService = jasmine.createSpyObj(["DEBUG"]);
-    mockVideoService = jasmine.createSpy();
+    mockVideoService = jasmine.createSpyObj(["uploadVideo"]);
 
     TestBed.configureTestingModule({
-      imports: [ FormsModule ],
-      declarations: [ VideoAddComponent ],
+      imports: [FormsModule],
+      declarations: [VideoAddComponent],
       providers: [
         { provide: VideoService, useValue: mockVideoService },
         { provide: LoggerService, useValue: mockLogService }
       ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -37,7 +38,7 @@ describe('VideoAddComponent', () => {
   });
 
   describe('htmlIframeOnBlur', () => {
-    it('should do nothing', () => {
+    it('should change nothing', () => {
       component.videoEntry.video_url = "dummy url";
       component.videoEntry.youtube_iframe = "<invalid iframe tag />";
 
@@ -59,6 +60,80 @@ describe('VideoAddComponent', () => {
       fixture.detectChanges();
 
       expect(component.videoEntry.video_url).toEqual(dummyUrl);
+    });
+
+    it('should leave video_url as is', () => {
+      let dummyUrl = "https://youtube.com/embed/fakevideo";
+      let dummyIframe = `<iframe dummy-tag another-tag="something"></iframe>`;
+      component.videoEntry.youtube_iframe = dummyIframe;
+      component.videoEntry.video_url = dummyUrl;
+
+      const videoIFrameText = fixture.debugElement.query(By.css("#db-form-video-iframe"));
+      videoIFrameText.triggerEventHandler("blur", {});
+      fixture.detectChanges();
+
+      expect(component.videoEntry.video_url).toEqual(dummyUrl);
+    });
+  });
+
+  describe('addTag', () => {
+    it("should change nothing", () => {
+      component.videoEntry.tags = ["#test1", "#test2", "#test3"];
+      component.newTagValue = "test1";
+
+      component.addTag();
+
+      expect(mockLogService.DEBUG).toHaveBeenCalledWith(
+        "VideoAddComponent.addTag",
+        `Tag "#test1" already exists`
+      );
+      expect(component.newTagValue).toBeNull();
+    });
+
+    it("should add new tag", () => {
+      component.videoEntry.tags = ["#test1", "#test2", "#test3"];
+      component.newTagValue = "test4";
+
+      component.addTag();
+
+      expect(component.videoEntry.tags.length).toEqual(4);
+    });
+  });
+
+  describe('removeTag', () => {
+    it("should remove specified tag", () => {
+      component.videoEntry.tags = ["#test1", "#test2", "#test3"];
+
+      component.removeTag(1);
+
+      expect(component.videoEntry.tags).toEqual(["#test1", "#test3"]);
+    });
+  });
+
+  describe('clearForm', () => {
+    it("should clear form", () => {
+      component.videoEntry.tags = ["#test1", "#test2"];
+      component.videoEntry.video_url = "https://youtube.com/embed/dummy";
+
+      component.clearForm();
+
+      expect(component.videoEntry.tags.length).toEqual(0);
+      expect(component.videoEntry.video_url).toBeNull();
+    });
+  });
+
+  describe('onSubmit', () => {
+    it("should clear form", () => {
+      component.videoEntry.video_url = "https://youtube.com/embed/dummy";
+      component.videoEntry.hero = "dva";
+      component.videoEntry.youtube_iframe = "<iframe />";
+      component.videoEntry.video_date = "2020-02-02";
+      mockVideoService.uploadVideo.and.returnValue(of({}));
+      fixture.detectChanges();
+
+      component.onSubmit();
+
+      expect(component.videoEntry.video_url).toBeNull();
     });
   });
 });
